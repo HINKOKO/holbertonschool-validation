22de6bf060f90cb7e8a5f8208a09982be9f635ce
root <piotr62@live.fr>
2023-01-04 08:54:38 -0800

    modifying setup

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-# spawn a sandboxed environment (Ubuntu 18.04)
-docker run --rm --tty --interactive --volume=$(pwd):/app --workdir=/app ubuntu:18.04 /bin/bash
-
 # Installation of Go-Hugo and Make command
 apt-get update && apt-get install -y hugo make
 # fixing the recipe for 'build' target fail
-brew install hugo
-
+# getting latest release of 'hugo'
+wget https://github.com/gohugoio/hugo/releases/download/v0.79.0/hugo_0.79.0_Linux-64bit.deb
+# install it
+dpkg -i hugo_0.79.0_Linux-64bit.deb
 # Running command build to build the website
 make build
