2d7f629d465aa06e546eb0a2e111b64c6cdf9600
root <piotr62@live.fr>
2023-01-06 02:41:22 -0800

    updating setup to try

--- module3_task2/setup.sh
+++ module3_task2/setup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Installation of Go-Hugo and Make command
-apt-get update && apt-get install -y hugo make wget
 # fixing the recipe for 'build' target fail
 # getting latest release of 'hugo'
 wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb	
