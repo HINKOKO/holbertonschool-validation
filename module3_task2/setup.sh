e557b43ef11dda868a08e95b256982df8d8628a5
root <piotr62@live.fr>
2023-01-06 02:57:06 -0800

    revert errors

--- module3_task2/setup.sh
+++ module3_task2/setup.sh
@@ -5,3 +5,4 @@
 wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb	
 # install it
 sudo dpkg -i hugo_extended_0.109.0_linux-amd64.deb	
+sudo apt install markdownlinter
