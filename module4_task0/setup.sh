bf3c3cc13db00137da8df9b54eea186832dce6a9
root <piotr62@live.fr>
2023-01-12 07:33:14 -0800

    changind lint chocolate lindt

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -2,5 +2,4 @@
 rm -rf /usr/local/go
 curl -Lo install_hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
 sudo apt-get install ./install_hugo.deb
-npm install -g markdownlint-cli
 
