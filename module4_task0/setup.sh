9e2a88c7ed58227175a59493e3d51ef7fdbd6e06
root <piotr62@live.fr>
2023-01-12 07:11:19 -0800

    let's go >Houssem

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 rm -rf /usr/local/go
+apt-get update && apt-get install -y wget npm
+
 # fixing the recipe for 'build' target fail
 # getting latest release of 'hugo'
 wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb	
