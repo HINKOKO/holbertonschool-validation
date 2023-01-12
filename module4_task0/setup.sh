dc2f041429337cddb899fb7154c545fb2ac48bfd
root <piotr62@live.fr>
2023-01-12 07:09:26 -0800

    let's go >Houssem

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-apt-get update && apt-get install -y hugo make wget npm
-
+rm -rf /usr/local/go
 # fixing the recipe for 'build' target fail
 # getting latest release of 'hugo'
 wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb	
 # install it
-sudo dpkg -i hugo_extended_0.109.0_linux-amd64.deb	
+sudo dpkg -i hugo_extended_0.109.0_linux-amd64.deb	  
 npm install -g markdownlint-cli
