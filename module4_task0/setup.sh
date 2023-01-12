245ed2434a4e974cb48bf0850d3be02889340974
root <piotr62@live.fr>
2023-01-12 07:20:47 -0800

    no way what's going on

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 rm -rf /usr/local/go
-apt-get update && apt-get install -y wget npm
-
-# fixing the recipe for 'build' target fail
-# getting latest release of 'hugo'
-wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb	
-# install it
-sudo dpkg -i hugo_extended_0.109.0_linux-amd64.deb	  
-npm install -g markdownlint-cli
+curl -Lo install_hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
+sudo apt-get install ./install_hugo.deb
