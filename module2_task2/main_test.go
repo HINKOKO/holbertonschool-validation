3cb32641a2d3551be5f6b899521249f01a5c3918
root <piotr62@live.fr>
2023-01-19 03:19:32 -0800

    final test

--- module2_task2/main_test.go
+++ module2_task2/main_test.go
@@ -47,6 +47,11 @@ func Test_HelloHandler(t *testing.T) {
       queryString:   "name=",
       responseCode:  400,
     },
+    {
+      name:         "finalSword",
+      queryString:   "nope",
+      responseCode:   400,
+    },
   }
   for _, tt := range tests {
     t.Run(tt.name, func(t *testing.T) {
